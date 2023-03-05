e9561e6af0888a9549814a1efe96720c67c97170
EC2 Default User <ec2-user@ip-172-31-24-207.ap-northeast-1.compute.internal>
2023-03-05 07:30:38 +0000

    Finish

--- db/migrate/20230225063707_add_profile_image_to_users.rb
+++ db/migrate/20230225063707_add_profile_image_to_users.rb
@@ -1,6 +1,5 @@
 class AddProfileImageToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :profile_image_id, :text
-    add_index :users, :name, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2023_03_04_072800) do
     t.integer "book_id"
     t.text "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
